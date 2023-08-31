0b6101eb4a83513bf4d593bdd251ada2f2155390
Vedank Pande <vedank.pande@gmail.com>
2023-08-31 17:22:58 +0530

    rough commit - ignore

--- src/scrapers/postScraper.py
+++ src/scrapers/postScraper.py
@@ -2,24 +2,25 @@ import os
 
 import praw
 
+from .baseScraper import BaseScraper
 
 
-class PostScraper:
+class PostScraper(BaseScraper):
     """
     Class to retrieve posts from a subreddit 
     """
-    def __init__(self) -> None:
-        pass
+    def __init__(self, reddit: praw.Reddit) -> None:
+        super().__init__(reddit)
+    
+    def get_submissions(self, subreddit: str, search_term: str):
 
-    # def get_submissions(subreddit, search_term):
+        sub = self.reddit.subreddit(subreddit)
 
-    #     subreddit = reddit.subreddit(subreddit)
+        posts = sub.search(
+            query = search_term,
+            sort = "hot",
+            time_filter = "week",
+            limit = 10,
+        )
 
-    #     posts = subreddit.search(
-    #         query = search_term,
-    #         sort = "hot",
-    #         time_filter = "week",
-    #         limit = 10,
-    #     )
-
-    #     return posts
+        return posts
