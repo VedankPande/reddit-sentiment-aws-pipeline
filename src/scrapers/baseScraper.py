dbcf30fa0fefecf25012b6ac16cea766551a250a
Vedank Pande <vedank.pande@gmail.com>
2023-08-31 17:21:42 +0530

    add last accessed property to super scraper class

--- src/scrapers/baseScraper.py
+++ src/scrapers/baseScraper.py
@@ -9,6 +9,19 @@ class BaseScraper:
     def __init__(self,reddit: Reddit) -> None:
         
         self.reddit = reddit
+        self._last_accessed = ""
+    
+    @property
+    def last_accessed(self) -> str:
+
+        return self._last_accessed
+
+    @last_accessed.setter
+    def last_accessed(self, value) -> None:
+        if not isinstance(value,str):
+            raise TypeError("Last accessed must be a string")
+        self._last_accessed = value
+
     
 
 
