77895c80054300c3ad719b73d45bf196716fa5ca
Vedank Pande <vedank.pande@gmail.com>
2023-09-03 15:33:39 +0530

    lambda handler functions initial commit

---
+++ src/lambda_handlers/lambda_scraper.py
@@ -0,0 +1,7 @@
+"""
+Lambda handler to get reddit data and create tree structure
+"""
+
+def lambda_handler(event,context):
+
+    return None
---
+++ src/lambda_handlers/lambda_sentiment.py
@@ -0,0 +1,7 @@
+"""
+Lambda handler used to get sentiment score for posts using Comprehend
+"""
+
+def lambda_handler(event,context):
+
+    return None
