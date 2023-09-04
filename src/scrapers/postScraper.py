fbd4ae20d3b7ff158519978cc7c997672999cde2
Vedank Pande <vedank.pande@gmail.com>
2023-09-04 17:45:22 +0530

    add limit arg to get_submissions

--- src/scrapers/postScraper.py
+++ src/scrapers/postScraper.py
@@ -12,15 +12,15 @@ class PostScraper(BaseScraper):
     def __init__(self, reddit: praw.Reddit) -> None:
         super().__init__(reddit)
     
-    def get_submissions(self, subreddit: str, search_term: str):
+    def get_submissions(self, subreddit: str, search_term: str, limit: int = 100):
 
         sub = self.reddit.subreddit(subreddit)
 
         posts = sub.search(
             query = search_term,
             sort = "hot",
-            time_filter = "week",
-            limit = 10,
+            #time_filter = "week",
+            limit = limit,
         )
 
         return posts
