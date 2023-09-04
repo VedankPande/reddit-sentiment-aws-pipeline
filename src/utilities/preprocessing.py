c4f21f481661b5365f2b7dcd4a03a6a85eb89cb4
Vedank Pande <vedank.pande@gmail.com>
2023-09-04 17:47:46 +0530

    add preprocessing functions for comments

--- src/utilities/preprocessing.py
+++ src/utilities/preprocessing.py
@@ -1,3 +1,19 @@
 """
 Preprocess comments to obtain accurate results from comprehend
 """
+
+import emoji
+import regex as re
+
+def clean_tweet(comment)->str:
+    '''
+    Removes links and special characters from text
+    '''
+    
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", comment).split()) 
+
+def clean_emojis(comment):
+    """
+    remove or replace emojis with corresponding text
+    """
+    return emoji.demojize(comment,delimiters=("",""))
