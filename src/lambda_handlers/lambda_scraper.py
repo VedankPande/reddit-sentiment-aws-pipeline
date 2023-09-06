b3c9658243ff43001c2b677ecd30b989477b5876
Vedank Pande <vedank.pande@gmail.com>
2023-09-06 14:51:02 +0530

    delete auth

--- src/lambda_handlers/lambda_scraper.py
+++
@@ -1,7 +0,0 @@
-"""
-Lambda handler to get reddit data and create tree structure
-"""
-
-def lambda_handler(event,context):
-
-    return None
--- src/lambda_handlers/lambda_sentiment.py
+++
@@ -1,7 +0,0 @@
-"""
-Lambda handler used to get sentiment score for posts using Comprehend
-"""
-
-def lambda_handler(event,context):
-
-    return None
